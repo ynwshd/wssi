21119bcc26371cbf8ae53e2cd53e546624d2c6ff
ynwshd <ynwshd@qq.com>
2018-05-10 17:34:21 +0800

    修改一些页面和controller

--- src/main/java/blog/controller/AdminController.java
+++ src/main/java/blog/controller/AdminController.java
@@ -5,10 +5,19 @@
  */
 package blog.controller;
 
-import core.controller.BaseController;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import blog.service.IBlogService;
+import blog.service.IUserService;
+import core.controller.BaseController;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  *
@@ -19,8 +28,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class AdminController extends BaseController {
 
     @ResponseBody
+    @RequestMapping(value = "/admin")
+    public ModelAndView Admin() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("admin");
+        return modelAndView;
+    }
+       @ResponseBody
     @RequestMapping(value = "/index")
-    public String Index() {
-        return "admin";
+    public ModelAndView Index() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("admin/index");
+        return modelAndView;
     }
 }
